ff9f5e3732708cdcb9b748a35972cde0a10def28
Mani <lmssrinivas@users.noreply.github.com>
2017-03-07 22:02:24 -0800

    Update server.js

--- server.js
+++ server.js
@@ -4,11 +4,11 @@
 
 var express = require('express');
 var path = require('path');
-
+var port = process.env.PORT || 3000;
 var app = express();
 
 app.use(express.static(path.join(__dirname,'public')))
 
-app.listen(3000,function (req,res) {
+app.listen(port,function (req,res) {
     console.log('request received');
 })
